c95a8efa190a37991b4037ae7072a69cb569833d
Satan73 <111057861+Satan73@users.noreply.github.com>
2024-01-05 19:34:05 +0545

    Conditions

---
+++ conditions.py
@@ -0,0 +1,13 @@
+# Write a Python program that takes an integer as input and prints
+# "Even" if it's an even number and "Odd" if it's an odd number
+
+# take input
+number = int(input("Enter any number:: "))
+if(number % 2 == 0):
+    print("even")
+else:
+    print("odd")
+
+#Create a program that asks the user for their age and checks if they are a child (0-12),
+    #  a teenager (13-19), an adult (20-59), or a senior (60+).
+    #
